d30c40af69e1f3d032722879f31c33dad4be96f3
Dan51122 <daniel5011ug@gmail.com>
2026-05-13 08:05:20 -0500

    Ya acabe

--- src/App.java
+++ src/App.java
@@ -1,9 +1,13 @@
+import controllers.CarController;
 import models.Car;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        CarController controller = new CarController();
+        Car
     }
+
 
     public static Car[] makeCarsList() {
         Car[] cars = new Car[20];
