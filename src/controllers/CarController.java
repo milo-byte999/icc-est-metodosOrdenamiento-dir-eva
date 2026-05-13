789cbbb269e5b41f2e083bed5650a831f2f026c6
Dan51122 <daniel5011ug@gmail.com>
2026-05-13 08:30:37 -0500

    Ya acabexd2

--- src/controllers/CarController.java
+++ src/controllers/CarController.java
@@ -1,10 +1,24 @@
 package controllers;
+
 import models.Car;
 
 public class CarController {
 
-    public void sortByName(Car[] cars){
-
+    public void sortByName(Car[] cars) {
+        int n = cars.length;
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (cars[j].getName().compareToIgnoreCase(cars[j + 1].getName()) > 0) {
+                    Car temp = cars[j];
+                    cars[j] = cars[j + 1];
+                    cars[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break; 
+            }
+        }
     }
-    
 }
